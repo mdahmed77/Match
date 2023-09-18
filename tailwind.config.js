dd87f14313353cc648b5fd426a12c4ff95ea5458
Daniyal Ahmed <daniyal.714k@gmail.com>
2023-09-18 14:30:22 +0500

    Create a Venue Page Designed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+      
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '992px',
+      // => @media (min-width: 992px) { ... }
+
+      'xl': '1200px',
+      // => @media (min-width: 1200px) { ... }
+
+      '2xl': '1480px',
+      // => @media (min-width: 1480px) { ... }
+    },
     extend: {},
   },
   plugins: [],
