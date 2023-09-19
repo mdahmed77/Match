8c9a5e125ac1737e211a6df7eeb85b9756fc2624
Daniyal Ahmed <daniyal.714k@gmail.com>
2023-09-19 21:26:02 +0500

    Design Fix

--- src/assets/js/theme.js
+++ src/assets/js/theme.js
@@ -22,9 +22,18 @@ $(document).ready(function () {
             $("#" + pillName).removeClass('hidden').addClass("block");
         });
     });
-    $('#toggleUserDrop').on('click', function(){
-        $('#userDrop').hasClass('invisible') 
-        ? $('#userDrop').removeClass('invisible opacity-0 translate-y-3').addClass('visible opacity-1 translate-y-0')
-        : $('#userDrop').addClass('invisible opacity-0 translate-y-3').removeClass('visible opacity-1 translate-y-0');
+    $('#toggleUserDrop').on('click', function () {
+        $('#userDrop').hasClass('invisible')
+            ? $('#userDrop').removeClass('invisible opacity-0 translate-y-3').addClass('visible opacity-1 translate-y-0')
+            : $('#userDrop').addClass('invisible opacity-0 translate-y-3').removeClass('visible opacity-1 translate-y-0');
+    });
+    $('.toggleSidebarDropdown').each(function () {
+        $(this).on('click', function () {
+            var sidebarDrop = $(this).parent().next();
+            var dropCaret = $(this).find('img');
+            sidebarDrop.hasClass('h-0') 
+            ? sidebarDrop.removeClass('h-0').addClass('h-auto') && dropCaret.addClass('rotate-180') 
+            : sidebarDrop.removeClass('h-auto').addClass('h-0') && dropCaret.removeClass('rotate-180')
+        });
     });
 });
