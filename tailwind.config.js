c4904bfcf30bb7288c6f562546d6820b9f59b1d1
Daniyal Ahmed <45146013+mdahmed77@users.noreply.github.com>
2024-03-25 19:32:20 +0500

    Initials

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     screens: {
       'sm': '640px',
       // => @media (min-width: 640px) { ... }
-      
+
       'md': '768px',
       // => @media (min-width: 768px) { ... }
 
@@ -18,8 +18,46 @@ module.exports = {
       '2xl': '1480px',
       // => @media (min-width: 1480px) { ... }
     },
-    extend: {},
+    extend: {
+      colors: {
+        'mango': '#FEBB24',
+        'zero': '#003FBC',
+        'indigo': '#6E81B6',
+        'violet': '#CF99FF',
+        'night': '#020842',
+        'shell': '#FFF8F4',
+      },
+      // fontFamily: {
+      //   tommy: ['MADE TOMMY', 'sans-serif'],
+      // },
+    },
   },
-  plugins: [],
+  corePlugins: {
+    container: false
+  },
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '600px',
+          },
+          '@screen md': {
+            maxWidth: '745px',
+          },
+          '@screen lg': {
+            maxWidth: '965px',
+          },
+          '@screen xl': {
+            maxWidth: '1180px',
+          },
+          '@screen 2xl': {
+            maxWidth: '1460px',
+          },
+        }
+      })
+    }
+  ]
 }
 
