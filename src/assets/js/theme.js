788c9375b911d7e551ef4cddc3398798922c62b0
Daniyal Ahmed <45146013+mdahmed77@users.noreply.github.com>
2024-04-02 11:48:09 +0500

    First Push

--- src/assets/js/theme.js
+++ src/assets/js/theme.js
@@ -1,39 +1,18 @@
 $(document).ready(function () {
-    var menu = $('#sideBar');
-    var backdrop = $('.sidebarOverlay');
-    $('#sidebarOpener').on('click', function () {
-        menu.removeClass('-translate-x-full').addClass('translate-x-0');
-        backdrop.removeClass('invisible').addClass('visible');
-        $('#sidebarCloser').removeClass('opacity-0 right-0 invisible').addClass('opacity-100 -right-5 visible');
-    });
-    $('#sidebarCloser, .sidebarOverlay').on('click', function () {
-        menu.removeClass('translate-x-0').addClass('-translate-x-full');
-        backdrop.addClass('invisible').removeClass('visible');
-        $('#sidebarCloser').removeClass('opacity-100 -right-5 visible').addClass('opacity-0 right-0 invisible');
-    });
-    $('.pill').each(function () {
+    $('.multiSelectDropper span').each(function () {
         $(this).on('click', function () {
-            var pillName = $(this).attr("data-pill");
-            if ($('.pill').hasClass('pill-active')) {
-                $('.pill').removeClass('pill-active');
-                $(this).addClass('pill-active');
-            }
-            $('.pill-tab.block').removeClass('block').addClass('hidden');
-            $("#" + pillName).removeClass('hidden').addClass("block");
+            $(this).hasClass('active')
+                ? $(this).removeClass('active')
+                : $('.multiSelectDropper span').removeClass('active') && $(this).addClass('active');
         });
     });
-    $('#toggleUserDrop').on('click', function () {
-        $('#userDrop').hasClass('invisible')
-            ? $('#userDrop').removeClass('invisible opacity-0 translate-y-3').addClass('visible opacity-1 translate-y-0')
-            : $('#userDrop').addClass('invisible opacity-0 translate-y-3').removeClass('visible opacity-1 translate-y-0');
-    });
-    $('.toggleSidebarDropdown').each(function () {
-        $(this).on('click', function () {
-            var sidebarDrop = $(this).parent().next();
-            var dropCaret = $(this).find('img');
-            sidebarDrop.hasClass('h-0') 
-            ? sidebarDrop.removeClass('h-0').addClass('h-auto') && dropCaret.addClass('rotate-180') 
-            : sidebarDrop.removeClass('h-auto').addClass('h-0') && dropCaret.removeClass('rotate-180')
+    $(document).ready(function(){
+        $(".tab-link").click(function(){
+            $(".tabs .tab-link").removeClass("active"); // Remove active class from all tab links
+            $(this).addClass("active");
+            $(".tab-content").removeClass("block").addClass('hidden');
+            var tabId = $(this).attr("data-tab");
+            $("#" + tabId).addClass("block").removeClass("hidden");
         });
     });
 });
